86ce28b6065e437933c1f918beba54296e81b430
Belvies-tech <theophilusblessing081@gmail.com>
2023-06-19 12:34:18 -0700

    my 0x01-variables_if_else_while file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /*
  * main - Entry point
- * Description - 'get the last digit of a number'
+ * Description: 'get the last digit of a number'
  * Return: Always 0
  */
 int main(void)
