9d46925cd9a09dd9ef77ebc6fa35225e22fe3763
Belvies-tech <theophilusblessing081@gmail.com>
2023-06-25 18:19:30 -0700

    my 0x04-more_functions_nested_loops file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int x;
 
 for (x = 1; x <= 100; x++)
 {
-if ((x % 3 ==0) && (x % 5 == 0))
+if ((x % 3 == 0) && (x % 5 == 0))
 {
 printf("FizzBuzz");
 }
@@ -35,5 +35,5 @@ printf(" ");
 }
 }
 _putchar('\n');
-return(0);
+return (0);
 }
