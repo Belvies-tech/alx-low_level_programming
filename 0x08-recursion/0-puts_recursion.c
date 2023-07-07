4f85d6048331f264016492cebf4ff28dfced7577
Belvies-tech <theophilusblessing081@gmail.com>
2023-07-06 19:04:21 -0700

    my 0x08-recursion file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _put_recursion - Print a string followed by a new line
+ * _puts_recursion - Print a string followed by a new line
  * @s: The string to print
  *
  * Return: Nothing.
