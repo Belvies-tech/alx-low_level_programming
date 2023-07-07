7cb809fc9678baccb05ca0bf3ae845b6ff3a09c4
Belvies-tech <theophilusblessing081@gmail.com>
2023-07-06 20:23:32 -0700

    my 0x08-recursion file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: string
+ * Return: The length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smaller iterator.
+ * @n2: bigger iterator.
+ * Return: . 
+ */
+int comparator(char *s, n1, n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 +1, n2 - 1));
+	}
+	return (0);
+}
