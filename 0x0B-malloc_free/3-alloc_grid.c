13c8dd5ac46e923e545b3c31ecccd306053df8bf
Belvies-tech <theophilusblessing081@gmail.com>
2023-07-12 17:37:34 -0700

    my 0x0B-malloc_free filr

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i =0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(sizeof(int) * width);
 
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 		{
 			for (j = i; j >= 0; j--)
 			{
-				free (a[j]);
+		free (a[j]);
 			}
 
 			free(a);
